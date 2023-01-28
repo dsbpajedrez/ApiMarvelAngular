68e360662807bca5faaa94bd9d819fb501ab9f90
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-01-28 14:23:43 -0500

    Refactor: Abstraccion de character.component.html

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CharacterDTO } from 'src/app/models/character.models';
 
 @Component({
   selector: 'app-character',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./character.component.scss']
 })
 export class CharacterComponent {
-    
+  @Input() character!: CharacterDTO
 }
